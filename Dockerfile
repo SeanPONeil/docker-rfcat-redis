997fc3c9755b83a0888c9c2608504fe1d9c6d508
Sean O'Neil <oneilse14@gmail.com>
2020-03-29 18:36:29 -0700

    Add rfcat setup

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,5 @@
 FROM seanponeil/rfcat
 
-ENV REDIS_HOST redis
-ENV REDIS_PORT 6379
-ENV BIND_PORT 5000
-EXPOSE $BIND_PORT
-
 RUN apt-get update
 RUN apt-get upgrade -y
 
--- app.py
+++ app.py
@@ -1,9 +1,24 @@
+import bitstring
 import os
+from rflib import *
 from redis import Redis
+from dotenv import load_dotenv
 
-REDIS_HOST = os.environ['REDIS_HOST']
-REDIS_PORT = os.environ['REDIS_PORT']
+load_dotenv()
 
+REDIS_HOST = os.getenv("REDIS_HOST", "redisdb")
+REDIS_PORT = os.getenv("REDIS_PORT", 6379)
+
+def setup_rfcat(debug):
+    if debug:
+        d = RfCat()
+        d.setMdmModulation(MOD_ASK_OOK)
+        d.setFreq(RFCAT_FREQ)
+        d.setMdmSyncMode(RFCAT_MDM_SYNC_MODE)
+        d.setMdmDRate(RFCAT_BAUD)
+        return d
+
+#setup_rfcat()
 redis = Redis(host=REDIS_HOST, port=REDIS_PORT)
 
 p = redis.pubsub()
